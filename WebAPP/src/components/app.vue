38655b27e21a76d3ee32aedb3ac5653a633d4d14
Stefano Malcangi <stefano.malcangi@away.travel>
2023-05-06 11:06:40 +0200

    Styling WIP

--- WebAPP/src/components/app.vue
+++ WebAPP/src/components/app.vue
@@ -1,29 +1,98 @@
 <template>
     <f7-app v-bind="f7params">
 
-        <!-- Views/Tabs container -->
-        <f7-views tabs class="safe-areas">
-            <!-- Tabbar for switching views-tabs -->
-            <f7-toolbar tabbar icons bottom>
-                <f7-link tab-link="#view-home" tab-link-active icon-ios="f7:house_fill" icon-md="material:home" text="Home"></f7-link>
-                <f7-link tab-link="#view-collaborators" icon-ios="f7:square_list_fill" icon-md="material:view_list" text="Collaboratos"></f7-link>
-                <f7-link tab-link="#view-equipments" icon-ios="f7:gear" icon-md="material:view_list" text="Equipments"></f7-link>
-                <f7-link tab-link="#view-permits" icon-ios="f7:gear" icon-md="material:view_list" text="Permits"></f7-link>
-            </f7-toolbar>
+        <div style="width:100%" class="display-flex">
+            <div class="section-left bg-custom-color-1">
 
-            <!-- Your main view/tab, should have "view-main" class. It also has "tab-active" class -->
-            <f7-view id="view-home" main tab tab-active url="/"></f7-view>
+                <div class="navigator-logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="237.751" height="42.368" viewBox="0 0 237.751 42.368">
+                        <defs>
+                            <clipPath id="clip-path">
+                                <rect id="Rettangolo_30" data-name="Rettangolo 30" width="171.502" height="31.976" transform="translate(0 0)" fill="#eee" />
+                            </clipPath>
+                        </defs>
+                        <g id="Raggruppa_28" data-name="Raggruppa 28" transform="translate(-27.001 -46)">
+                            <g id="Raggruppa_13" data-name="Raggruppa 13" transform="translate(-14 21.828)">
+                                <path id="Tracciato_24" data-name="Tracciato 24" d="M25,0,0,42.368H50Z" transform="translate(41 24.172)" />
+                                <path id="Tracciato_29" data-name="Tracciato 29" d="M13.16,10.352A2.959,2.959,0,1,1,10.2,7.394a2.958,2.958,0,0,1,2.958,2.958" transform="translate(55.799 39.577)" fill="#fff" />
+                            </g>
+                            <g id="Raggruppa_19" data-name="Raggruppa 19" transform="translate(93.25 52.196)">
+                                <g id="Raggruppa_18" data-name="Raggruppa 18" clip-path="url(#clip-path)">
+                                    <path id="Tracciato_30" data-name="Tracciato 30" d="M26.618,13.2a12.174,12.174,0,0,1-12.13,12.13v5.7H0v-30H14.359A12.211,12.211,0,0,1,26.618,13.2" fill="#eee" />
+                                    <path id="Tracciato_31" data-name="Tracciato 31" d="M59.581,15.987A15.988,15.988,0,1,1,43.593,0,15.988,15.988,0,0,1,59.581,15.987" fill="#eee" />
+                                    <path id="Tracciato_32" data-name="Tracciato 32" d="M82.3,31.075h-15v-15H61.084V1.029H88.13V16.073H82.3Z" fill="#eee" />
+                                    <rect id="Rettangolo_29" data-name="Rettangolo 29" width="15.602" height="30.003" transform="translate(90.275 1.03)" fill="#eee" />
+                                    <path id="Tracciato_33" data-name="Tracciato 33" d="M139.567,15.987A15.987,15.987,0,1,1,123.58,0a15.988,15.988,0,0,1,15.987,15.987" fill="#eee" />
+                                    <path id="Tracciato_34" data-name="Tracciato 34" d="M156.586,12.087V1.029H171.5v30h-30V.3Z" fill="#eee" />
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                </div>
 
-            <!-- Collaborators View -->
-            <f7-view id="view-collaborators" name="collaborators" tab url="/collaborators/"></f7-view>
+                <f7-list>
+                    <f7-list-item>
+                        <template #media>
+                            <div class="navigator-item-icon active">
+                                <f7-icon f7="square_grid_2x2" />
+                            </div>
+                        </template>
+                        <template #title>
+                            <f7-link class="navigator-item-link active" tab-link="#view-home" tab-link-active text="Dashboard"></f7-link>
+                        </template>
+                    </f7-list-item>
+                    <f7-list-item>
+                        <template #media>
+                            <div class="navigator-item-icon">
+                                <f7-icon f7="square_arrow_left" />
+                            </div>
+                        </template>
+                        <template #title>
+                            <f7-link class="navigator-item-link" tab-link="#view-permits" tab-link-active text="Permits"></f7-link>
+                        </template>
+                    </f7-list-item>
+                    <f7-list-item>
+                        <template #media>
+                            <div class="navigator-item-icon">
+                                <f7-icon f7="device_laptop" />
+                            </div>
+                        </template>
+                        <template #title>
+                            <f7-link class="navigator-item-link" tab-link="#view-equipments" tab-link-active text="Assets"></f7-link>
+                        </template>
+                    </f7-list-item>
+                    <f7-list-item>
+                        <template #media>
+                            <div class="navigator-item-icon">
+                                <f7-icon f7="person_2" />
+                            </div>
+                        </template>
+                        <template #title>
+                            <f7-link class="navigator-item-link" tab-link="#view-collaborators" tab-link-active text="Collaborators"></f7-link>
+                        </template>
+                    </f7-list-item>
+                </f7-list>
+            </div>
+            <div class="section-right bg-custom-color-2">
+                <!-- Views/Tabs container -->
+                <f7-views tabs class="safe-areas">
+                    <!-- Tabbar for switching views-tabs -->
+                    <!-- Your main view/tab, should have "view-main" class. It also has "tab-active" class -->
+                    <f7-view id="view-home" main tab tab-active url="/"></f7-view>
 
-            <!-- Equipments View -->
-            <f7-view id="view-equipments" name="equipments" tab url="/equipments/"></f7-view>
+                    <!-- Collaborators View -->
+                    <f7-view id="view-collaborators" name="collaborators" tab url="/collaborators/"></f7-view>
 
-            <!-- Permits View -->
-            <f7-view id="view-permits" name="permits" tab url="/permits/"></f7-view>
+                    <!-- Equipments View -->
+                    <f7-view id="view-equipments" name="equipments" tab url="/equipments/"></f7-view>
 
-        </f7-views>
+                    <!-- Permits View -->
+                    <f7-view id="view-permits" name="permits" tab url="/permits/"></f7-view>
+
+                </f7-views>
+            </div>
+        </div>
+
     </f7-app>
 </template>
 <script>
@@ -41,6 +110,10 @@
             const f7params = {
                 name: 'Alkimia Potion', // App name
                 theme: 'auto', // Automatic theme detection
+                colors: {
+                    // specify primary color theme
+                    primary: '#419DA4'
+                },
 
                 // App store
                 store: store,
--- WebAPP/src/pages/collaborators.vue
+++ WebAPP/src/pages/collaborators.vue
@@ -1,25 +1,101 @@
 <template>
     <f7-page name="collaborators">
-        <f7-navbar title="Collaborators"></f7-navbar>
-        <f7-list strong dividers-ios outline-ios inset-md>
-            <f7-list-item v-for="collaborator in collaborators"
-                          :key="collaborator.id"
-                          :title="collaborator.name + ' ' + collaborator.surname"
-                          :link="`/collaborator/${collaborator.id}/`"></f7-list-item>
-        </f7-list>
-        <f7-block>
-            <f7-button fill small @click="addCollaborator">Add Collaborator</f7-button>
-        </f7-block>
+
+        <div class="page-content-wrapper">
+            <div class="display-flex justify-content-space-between">
+                <h1>Collaborators</h1>
+                <div class="margin-vertical margin-left">
+                    <f7-button fill @click="addCollaborator" color="white">
+                        <f7-icon f7="plus_circle" color="teal"></f7-icon>
+                        <strong class="display-inline-block padding-left-half">
+                            Add Collaborator
+                        </strong>
+                    </f7-button>
+                </div>
+            </div>
+
+            <div class="search-bar bg-custom-color-1 border-radius-12 elevation margin-bottom">
+                <div class="row padding border-bottom-solid-2">
+                    <div class="row-cell-80 display-flex align-items-center">
+                        <div class="navigator-item-icon margin-right bg-custom-color-1">
+                            <f7-icon f7="search" />
+                        </div>
+                        <div style="width:70%">
+                            <f7-input type="text"
+                                      placeholder="Search for Name, Surname, Email and Phone number"
+                                      class="input-blank"
+                                      clear-button />
+                        </div>
+                    </div>
+                    <div class="row-cell-20 text-align-right">
+                        <f7-button fill @click="addCollaborator" class="padding-horizontal display-inline-block">
+                            <strong class="display-inline-block">
+                                Search
+                            </strong>
+                        </f7-button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="bg-custom-color-1 border-radius-12 elevation">
+                <div class="row padding border-bottom-solid-2 row-hover" 
+                     
+                     v-for="collaborator in collaborators"
+                     :key="collaborator.id"
+                     @click="">
+                    <div class="row-cell-30 display-flex align-items-center">
+                        <div class="navigator-item-icon margin-right bg-custom-color-2">
+                            <f7-icon f7="person" />
+                        </div>
+                        <strong>
+                            {{collaborator.name + ' ' + collaborator.surname}}
+                        </strong>
+                    </div>
+                    <div class="row-cell-30">
+                        <f7-link>
+                            {{collaborator.enail}}</f7-link>
+                    </div>
+                    <div class="row-cell-30">
+                        {{collaborator.tel}}
+                    </div>
+                    <div class="row-cell-10 text-align-right">
+                        <f7-icon f7="chevron_right" color="gray"></f7-icon>
+                    </div>
+                </div>
+                <div class="row padding">
+                    <div class="row-cell-30">
+                        Items per page 
+                        <strong>25</strong>
+                    </div>
+                    <div class="row-cell-70 display-flex justify-content-flex-end align-items-center">
+                        <div class="page-number-item padding-horizontal padding-vertical-half">
+                            <f7-link icon-f7="chevron_left"></f7-link>
+                        </div>
+                        <div v-for="n in 5" class="page-number-item padding-horizontal padding-vertical-half">
+                            {{n}}
+                        </div>
+                        <div class="padding-left padding-vertical-half">
+                            <f7-link icon-f7="chevron_right"></f7-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--<f7-list strong dividers-ios outline-ios inset-md>
+                <f7-list-item v-for="collaborator in collaborators"
+                              :key="collaborator.id"
+                              :title="collaborator.name + ' ' + collaborator.surname"
+                              :link="`/collaborator/${collaborator.id}/`"></f7-list-item>
+            </f7-list>-->
+        </div>
     </f7-page>
 </template>
 <script>
     import { useStore } from 'framework7-vue';
     import store from '../js/store';
-
     export default {
         setup() {
             const collaborators = useStore('collaborators');
-
             const addCollaborator = () => {
                 store.dispatch('addCollaborator', {
                     "id": 0,
@@ -36,7 +112,6 @@
                     "note": ""
                 });
             };
-
             return {
                 collaborators,
                 addCollaborator

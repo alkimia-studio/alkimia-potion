dfdc34fdee9d0641a5a51775b93cab89636a83ce
Fabrizio Anagi <f.anagi@myw.ai>
2023-05-16 11:42:04 +0200

    fdsd

--- WebAPI/DTO/DTO.cs
+++ WebAPI/DTO/DTO.cs
@@ -5,7 +5,7 @@ namespace WebAPI.DTO
 {
     public class EquipmentsAndTypes
     {
-        public IEnumerable<Equipment>? Equipments { get; set; } = new List<Equipment>();
+        public IEnumerable<Equipment> Equipments { get; set; } = new List<Equipment>();
 
         public IEnumerable<EquipmentType> EquipmentTypes { get; set; } = new  List<EquipmentType>();
 
